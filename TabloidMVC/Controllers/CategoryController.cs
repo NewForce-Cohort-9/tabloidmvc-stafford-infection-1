b6dcfbd4943b3b48796e27ba63a9d62db813574f
Ben Bower <benbower44@gmail.com>
2024-08-29 14:22:45 -0400

    pulling main from tyler

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TabloidMVC.Repositories;
+using TabloidMVC.Models;
+using System;
+
+namespace TabloidMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepo;
+
+        // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepo = categoryRepository;
+        }
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            List<Category> categories = _categoryRepo.GetAll();
+            return View(categories);
+        }
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        
+        // GET: CategoryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Category category = _categoryRepo.GetCategoryById(id);
+            return View(category);
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Category category)
+        {
+            try
+            {
+                _categoryRepo.DeleteCategory(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View(category);
+            }
+        }
+    }
+}
+    
+   
