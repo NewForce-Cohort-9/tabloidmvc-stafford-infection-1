e2717b6c16d2f15dffc933f822c404acea5461cb
Fiona Wee <fwee@oswego.edu>
2024-08-29 09:07:06 -0400

    added view comments for post functionality

--- TabloidMVC/Models/UserProfile.cs
+++ TabloidMVC/Models/UserProfile.cs
@@ -1,10 +1,13 @@
-﻿namespace TabloidMVC.Models
+﻿using System.ComponentModel;
+
+namespace TabloidMVC.Models
 {
     public class UserProfile
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [DisplayName("Author")]
         public string DisplayName { get; set; }
         public string Email { get; set; }
         public DateTime CreateDateTime { get; set; }
---
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> GetAllComments();
+
+    }
+}
